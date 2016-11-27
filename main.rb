aac2bf2c062942e49115acf41b05ad71b8941cc2
Dharini Murugaprabhu <dharinishree@gmail.com>
2016-11-26 18:38:53 -0800

    Tables added in ruby file

--- main.rb
+++ main.rb
@@ -1,43 +1,28 @@
 require 'sinatra' 
-
 require 'slim'
 
+require './sql_db_creation.rb'
+
 get '/' do 
-  
-  @myvar = 5
-  @lists = [
-  	5,
-  	6,
-  	7,
-  	8
-  ]
-  slim :indexDo
+  slim :index
   # file = File.open("./index.html", "r")
   # contents = file.read
 end
 
-__END__
+get '/details' do
+  @height = params[:height]
+  @weight = params[:weight]
+  @age = params[:age]
+  slim :thanks
+end
 
-@@layout 
-doctype html 
-html
-  head 
-    meta charset="utf-8" 
-    title Just Do It 
-    link rel="stylesheet" media="screen, projection" href="/styles.css" 
-    /[if lt IE 9] 
-      script src="http://html5shiv.googlecode.com/svn/trunk/html5.js" 
-    javascript:
-      alert('Slim supports embedded javascript!')
-  body 
-    h1 Just Do It 
-    == yield
 
-@@indexDo 
-h2 My Tasks
-= @myvar
-ul.tasks
-  li Get Milk #{@myvar}
-ul.lists
-- @lists.each do |a|
-	li Wait is #{a}
+# sqldb = SqlDbCreator.new
+# sqldb.create
+# sqldb.fill_data
+
+# puts "Server has been started & data has been populated into the DB"
+
+
+
+
